3c116bfccb9a894753e72159e4651f18e2230f62
SmyZ1k <zmkllkmk123pro@gmail.com>
2026-03-17 20:05:55 +0300

    show command working

--- src/BaseFiles/Movie.java
+++ src/BaseFiles/Movie.java
@@ -3,7 +3,8 @@ package BaseFiles;
 import java.time.LocalDateTime;
 
 public class Movie {
-    private static int id;//Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private static int Nextid = 1;
+    private int id;//Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
     private java.time.LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
@@ -12,9 +13,9 @@ public class Movie {
     private int usaBoxOffice; //Значение поля должно быть больше 0
     private MpaaRating mpaaRating; //Поле не может быть null
     private Person screenwriter;
-     Movie() {};
      public Movie(String name) {
-        Movie.id+=1;
+        this.id = Nextid;
+        Movie.Nextid+=1;
         this.name = name;
         this.coordinates = new Coordinates(1.0, 1.0F);
         this.creationDate = LocalDateTime.now();
